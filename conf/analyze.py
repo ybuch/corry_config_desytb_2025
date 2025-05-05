0b809767fa10f815ad1aec28fa976f891538ca37
bgnet2 <you@example.com>
2025-05-05 17:56:30 +0200

    Change paths

--- conf/analyze.py
+++ conf/analyze.py
@@ -13,7 +13,7 @@ def runCorry(config, files, log, additional=None):
         log: Log file where the output will be saved.
         additional: Optional string with additional arguments to pass to the Corry program.
     """
-    cmd = f'./corry -c {config} -o EventLoaderEUDAQ2.file_name={files[0]} -o EventLoaderEUDAQ2:TLU_0.file_name={files[1]} -o EventLoaderHDF5.filename={files[2]} -l {log} -o EventLoaderMuPixTelescope.input_file={files[3]} -l {log} '
+    cmd = f'corry -c {config} -o EventLoaderEUDAQ2.file_name={files[0]} -o EventLoaderEUDAQ2:TLU_0.file_name={files[1]} -o EventLoaderHDF5.filename={files[2]} -l {log} -o EventLoaderMuPixTelescope.input_file={files[3]} -l {log} '
     if additional:
         cmd += additional
     print(cmd)  
@@ -38,9 +38,15 @@ def main():
     geo_dut_pre_aligned = geo[:-4] + "_dut_pre_aligned.geo"
 
     # Directory paths where the data is stored
-    telDir = 'data/telescope'
-    tluDir = 'data/tlu'
-    dutDir = 'data/dut'
+    telDir = '/s3_cloud/TB2025/desy-tb-2025/data/telescope'
+    tluDir = '/s3_cloud/TB2025/desy-tb-2025/data/tlu'
+    dutDir = '/s3_cloud/TB2025/desy-tb-2025/data/dut'
+    mask_file_mimosa26_0 = '/corry_config_desytb_2025/geo/mask_files/mask_MIMOSA26_0.txt'
+    mask_file_mimosa26_1 = '/corry_config_desytb_2025/geo/mask_files/mask_MIMOSA26_1.txt'
+    mask_file_mimosa26_2 = '/corry_config_desytb_2025/geo/mask_files/mask_MIMOSA26_2.txt'
+    mask_file_mimosa26_3 = '/corry_config_desytb_2025/geo/mask_files/mask_MIMOSA26_3.txt'
+    mask_file_mimosa26_4 = '/corry_config_desytb_2025/geo/mask_files/mask_MIMOSA26_4.txt'
+    mask_file_mimosa26_5 = '/corry_config_desytb_2025/geo/mask_files/mask_MIMOSA26_5.txt'
 
     # List of directories to glob for the files
     dirs = [telDir, tluDir]
@@ -59,8 +65,8 @@ def main():
     # Loop through the range of runs
     for runNmb in range(first, last + 1):
 
-        os.system(f'python /media/testbeam1/tb2025c/tb2025d/desy-tb-2025/find_masked_pixel_analysis.py {runNmb}')
-        mask_file = f'/media/testbeam1/tb2025c/tb2025d/desy-tb-2025/data/dut/module_0/chip_0/run{str(runNmb).zfill(6)}_masked_pixels.txt'
+        #os.system(f'python /s3_cloud/TB2025/desy-tb-2025/find_masked_pixel_analysis.py {runNmb}')
+        mask_file = f'/s3_cloud/TB2025/desy-tb-2025/data/dut/module_0/chip_0/run{str(runNmb).zfill(6)}_masked_pixels.txt'
 
         files = []
 
@@ -76,7 +82,7 @@ def main():
 
         # Globbing for the DUT file
         print(f'Globbing for DUT file with run number {runNmb:06}')
-        dut_file_found = glob(f'data/dut/module_0/chip_0/run{runNmb:06}_converted.h5')
+        dut_file_found = glob(f'/s3_cloud/TB2025/desy-tb-2025/data/dut/module_0/chip_0/run{runNmb:06}_converted.h5')
         if dut_file_found:
             files.append(dut_file_found[0])  # Append the first matched DUT file
         else:
@@ -85,7 +91,7 @@ def main():
 
         # Globbing for the telepix2 block file
         print(f'Globbing for telepix2 block file for run {runNmb:06}')
-        telepix_file_found = glob(f'data/telepix2/single_run_{runNmb:06}.blck')
+        telepix_file_found = glob(f'/s3_cloud/TB2025/desy-tb-2025/data/telepix2/single_run_{runNmb:06}.blck')
         if telepix_file_found:
             files.append(os.path.basename(telepix_file_found[0]))  # Append the block file
         else:
@@ -93,9 +99,9 @@ def main():
             sys.exit(1)
 
         # Run Corry with the found files and configuration
-        runCorry("align_dut.conf",files, f'logs/log_dut_align1{runNmb:06}.txt',f'-o histogram_file=dut_align_{runNmb:06}.root -o detectors_file={geo} -o number_of_tracks=50000 -o detectors_file_updated={geo_dut_pre_aligned} -o DUTAssociation.spatial_cut_abs=150um,150um -g Monopix2_0.mask_file={mask_file}')
-        runCorry("align_dut.conf",files, f'logs/log_dut_align2{runNmb:06}.txt',f'-o histogram_file=dut_align_{runNmb:06}.root -o detectors_file={geo_dut_pre_aligned} -o number_of_tracks=75000 -o detectors_file_updated={geo_dut_aligned} -o DUTAssociation.spatial_cut_abs=50um,50um -g Monopix2_0.mask_file={mask_file}')
-        runCorry(config, files, f'logs/log_ana{runNmb:06}.txt', f'-o histogram_file=analysis_{runNmb:06}.root -o detectors_file={geo_dut_aligned} -g Monopix2_0.mask_file={mask_file}')
+        runCorry("align_dut.conf",files, f'/corry_config_desytb_2025/logs/log_dut_align1{runNmb:06}.txt',f'-o histogram_file=dut_align_{runNmb:06}.root -o detectors_file={geo} -o number_of_tracks=50000 -o detectors_file_updated={geo_dut_pre_aligned} -o DUTAssociation.spatial_cut_abs=150um,150um -g Monopix2_0.mask_file={mask_file} -g MIMOSA26_0.mask_file={mask_file_mimosa26_0} -g MIMOSA26_1.mask_file={mask_file_mimosa26_1} -g MIMOSA26_2.mask_file={mask_file_mimosa26_2} -g MIMOSA26_3.mask_file={mask_file_mimosa26_3} -g MIMOSA26_4.mask_file={mask_file_mimosa26_4} -g MIMOSA26_5.mask_file={mask_file_mimosa26_5}')
+        runCorry("align_dut.conf",files, f'/corry_config_desytb_2025/logs/log_dut_align2{runNmb:06}.txt',f'-o histogram_file=dut_align_{runNmb:06}.root -o detectors_file={geo_dut_pre_aligned} -o number_of_tracks=75000 -o detectors_file_updated={geo_dut_aligned} -o DUTAssociation.spatial_cut_abs=50um,50um -g Monopix2_0.mask_file={mask_file} -g MIMOSA26_0.mask_file={mask_file_mimosa26_0} -g MIMOSA26_1.mask_file={mask_file_mimosa26_1} -g MIMOSA26_2.mask_file={mask_file_mimosa26_2} -g MIMOSA26_3.mask_file={mask_file_mimosa26_3} -g MIMOSA26_4.mask_file={mask_file_mimosa26_4} -g MIMOSA26_5.mask_file={mask_file_mimosa26_5}')
+        runCorry(config, files, f'/corry_config_desytb_2025/logs/log_ana{runNmb:06}.txt', f'-o histogram_file=analysis_{runNmb:06}.root -o detectors_file={geo_dut_aligned} -g Monopix2_0.mask_file={mask_file} -g MIMOSA26_0.mask_file={mask_file_mimosa26_0} -g MIMOSA26_1.mask_file={mask_file_mimosa26_1} -g MIMOSA26_2.mask_file={mask_file_mimosa26_2} -g MIMOSA26_3.mask_file={mask_file_mimosa26_3} -g MIMOSA26_4.mask_file={mask_file_mimosa26_4} -g MIMOSA26_5.mask_file={mask_file_mimosa26_5}')
 
 if __name__ == "__main__":
     main()
